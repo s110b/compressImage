9cfc225ec188f94bd4ea3c38cf40f3463c2d3fe9
s110b <111761629+s110b@users.noreply.github.com>
2023-11-27 10:49:21 +0800

    c
    c

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ try {
     const inputPath = core.getInput('input');
     const outputPath = core.getInput('output');
     const backupPath = core.getInput('backup');
+    
+    console.log(`Input Path: ${inputPath}`);
+    console.log(`Output Path: ${outputPath}`);
+    console.log(`Backup Path: ${backupPath}`);
 
+    // 调用 compressImages 函数
     // 调用 compressImages 函数
     compressImages(inputPath, outputPath, backupPath);
 } catch (error) {
